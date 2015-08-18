d9db9ac1ce48f47b417f1735b758612decf6b0aa
Vladimir <vova@dev1.local>
2015-08-18 13:39:35 +1000

    base recipes

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+default['app'] = 'projectxx'
+default['app_path'] = '/var/www/projectxx/current'
+
+default['nodejs']['dir'] = '/usr/local'
+default['nodejs']['version'] = '0.12.7'
+
+default['ruby']['version'] = '2.2.2'
+default['redis']['version'] = '3.0.3'
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,32 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# update package database
+execute 'apt-get update'
+
+# install packages
+package 'telnet'
+package 'postfix'
+package 'curl'
+package 'git-core'
+package 'zlib1g-dev'
+package 'libssl-dev'
+package 'libreadline-dev'
+package 'libyaml-dev'
+package 'libsqlite3-dev'
+package 'sqlite3'
+package 'libxml2-dev'
+package 'libxslt1-dev'
+package 'libpq-dev'
+package 'build-essential'
+package 'tree'
+
+# set timezone
+bash 'set timezone' do
+  code <<-EOH
+    echo 'Asia/Vladivostok' > /etc/timezone
+    dpkg-reconfigure -f noninteractive tzdata
+  EOH
+  not_if "date | grep -q 'VLAT\|VLAST'"
+end
