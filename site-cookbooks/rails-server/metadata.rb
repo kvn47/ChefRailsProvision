5e9d473b9c65cd527455dd84299a0b1e2a9e03d2
Vladimir <vova@dev1.local>
2015-09-03 18:01:08 +1000

    fix sidekiq template

--- site-cookbooks/rails-server/metadata.rb
+++ site-cookbooks/rails-server/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kvn047@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 supports 'ubuntu'
 
